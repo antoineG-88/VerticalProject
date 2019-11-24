934efb2158396a9cee5b4f3c0b3f48d547b4cd9b
ShaeRedwyn <manon.ghignoni@gmail.com>
2019-11-24 19:37:13 +0100

    Proto 25/11 tile map ajouté

---
+++ Vertical_Unity/Assets/Scripts/PicTrap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PicTrap : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        GameData.playerManager.TakeDamage(8, new Vector2(2f,13f),0);
+    }
+}
+
