5c75792188c6d7e1865cbdb1cd183aff1a55e978
ShaeRedwyn <manon.ghignoni@gmail.com>
2020-01-04 16:08:08 +0100

    HealTerminal ajouté

---
+++ Vertical_Unity/Assets/Scripts/HealTerminal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealTerminal : MonoBehaviour
+{
+    public int lifePrice;
+    GameObject interactButton;
+    public int lifeAdded;
+    bool isUsed;
+
+    private void Start()
+    {
+        isUsed = true;
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (isUsed == true)
+        {
+
+            if (Input.GetButton("Interact") == true)
+            {
+                GameData.playerManager.currentEnergy = GameData.playerManager.currentEnergy - lifePrice;
+                GameData.playerManager.currentHealth = GameData.playerManager.currentHealth + lifeAdded;
+                GameData.playerManager.UpdateHealthBar();
+                isUsed = false;
+            }
+        }
+    }
+}
